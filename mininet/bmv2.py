eae8c214f378934276cda97d5356eeac7a376ffd
David Caetano <davidcoimbra73@hotmail.com>
2025-01-26 18:02:07 +0000

    allow ipv6

--- mininet/bmv2.py
+++ mininet/bmv2.py
@@ -105,10 +105,6 @@ class ONOSHost(Host):
             cmd = "/sbin/ethtool --offload %s %s off" \
                   % (self.defaultIntf(), off)
             self.cmd(cmd)
-        # disable IPv6
-        self.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
-        self.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
-        self.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
         return r
 
 
